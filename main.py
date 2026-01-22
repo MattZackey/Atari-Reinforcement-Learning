02c9cb182e2f33ee9e085dd02fe0fc9716841996
Matthew Zackey <149171771+MattZackey@users.noreply.github.com>
2026-01-22 20:51:12 +0200

    Update main.py
    Removed comments

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def main(args):
     train_on_aws = str_to_bool(args.train_on_aws)
     enable_starter_action = str_to_bool(args.enable_starter_action)
     
-    # if args.save_frequency < 1000:
+    if args.save_frequency < 1000:
         
-    #     logging.error("Save frequency must be at least 1000.")
-    #     sys.exit(1)  
+        logging.error("Save frequency must be at least 1000.")
+        sys.exit(1)  
         
     if train_on_aws: # Setup on S3
         
